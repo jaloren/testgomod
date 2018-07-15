7c0a087a761da04b55295afb3fbf35439106827c
Joseph Lorenzini <jaloren@gmail.com>
2018-07-15 06:43:34 -0500

    add breaking import

--- oldvault/oldvault.go
+++ oldvault/oldvault.go
@@ -2,9 +2,11 @@ package oldvault
 
 import (
 	"github.com/hashicorp/vault/meta"
+	"github.com/jaloren/testgomod/newvault"
 )
 
 // Commands provides the functions that back locksmith functionality
 func Commands() *meta.Meta {
+	newvault.NewClient()
 	return &meta.Meta{}
 }
